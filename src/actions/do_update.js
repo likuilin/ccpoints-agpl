514b682c8a1c7a64d69ee7b326b34686496caee3
Kuilin Li <kuilin@gmail.com>
2022-02-20 08:38:39 +0000

    Fix update

--- src/actions/do_update.js
+++ src/actions/do_update.js
@@ -5,6 +5,7 @@ module.exports = async (req, res) => {
     const check = await requirePerm(req, "update", false);
     if (check !== true) return res.send(check);
 
+    execSync("mkdir -p ~/.ssh");
     execSync("ssh-keyscan github.com > ~/.ssh/known_hosts");
     execSync("chmod 400 /app/keys/deploykey");
     const ret = execSync("GIT_SSH_COMMAND=\"ssh -i /app/keys/deploykey\" git pull --ff-only -X theirs").toString();
